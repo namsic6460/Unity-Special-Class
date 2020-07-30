cc0a1f4847a26a135b075e6c229e64e66c94100b
user <namsic6460@gmail.com>
2020-07-30 09:50:11 +0900

    End

---
+++ Tetris/Assets/Scripts/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer == null)
+            Debug.LogError("spriteRenderer NPE");
+    }
+
+    public Color color
+    {
+        set
+        {
+            spriteRenderer.color = value;
+        }
+
+        get
+        {
+            return spriteRenderer.color;
+        }
+    }
+
+    public int sortingOrder
+    {
+        set {
+            spriteRenderer.sortingOrder = value;
+        }
+
+        get
+        {
+            return spriteRenderer.sortingOrder;
+        }
+    }
+
+}
